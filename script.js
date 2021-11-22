bd2d630cb8b35048ebe0376779399d4634bab96d
MateuszBialowas <mateusz.bialowas2@gmail.com>
2021-11-22 21:42:25 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function fetch_products_from_local_storage() {
+  var products = [];
+  var products_string = localStorage.getItem("products");
+  if (products_string) {
+    products = JSON.parse(products_string);
+  }
+  return products;
+}
+
+add_new_product_to_local_storage = function(product) {
+    var products = fetch_products_from_local_storage();
+    products.push(product);
+    localStorage.setItem("products", JSON.stringify(products));
+}
+
+console.log(fetch_products_from_local_storage());
